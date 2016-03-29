39febe047c209bc884b25470fa072c93a36c0e32
ansgml9884 <ansgml9884@naver.com>
2016-03-29 22:38:04 +0900

    assignment
    assignment

---
+++ w3Main.py
@@ -0,0 +1,9 @@
+﻿temp = input("user input temperature:")
+sel = input("F or C:")
+print (temp, sel)
+if(sel=='F'):
+    print (int((temp)-32)*5/9,'C')
+elif(sel=='C'):
+    print (int(temp)*9/5+32,'F')
+else:
+    print ("Input Error")
