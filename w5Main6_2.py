2c31bf73387b8f0e3790346a22d3c0166ab83ea6
ansgml9884 <ansgml9884@naver.com>
2016-04-03 23:07:41 +0900

    Added files via upload

---
+++ w5Main6_2.py
@@ -0,0 +1,20 @@
+﻿import turtle
+wn=turtle.Screen()
+
+userA=raw_input('userA input rock, scissors, paper : ')
+userB=raw_input('userB input rock, scissors, paper : ')
+
+def RSPgame():
+   if userA!=userB:
+      if userA=='rock' and userB=='scissors':
+         print 'A is winner'
+      elif userA=='scissors' and userB=='rock':
+         print 'A is winner'
+      elif userA=='paper' and userB=='rock':
+         print 'A is winner'
+      else:
+         print 'B is winner'
+   elif userA==userB:
+      print "Draw"
+RSPgame()
+wn.exitonclick()
