091c2db446dc54d7dc478278f9aa3f9b4052d9a8
김문희 201611057 <ansgml9884@naver.com>
2016-04-07 18:35:51 +0900

    Added files via upload

---
+++ w6Main11.py
@@ -0,0 +1,15 @@
+﻿def Leapyear():
+	year=input("User input year : ")
+        if (year%4 == 0) and (year%100 !=0 or year%400==0):
+            print "Leap year"
+        else:
+            print "Not Leap year"
+
+def lab6():
+	Leapyear()
+
+def main():
+	lab6()
+
+if __name__=="__main__":
+	main()
