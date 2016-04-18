696b92cf9cb931978f113e91cb13b6cd31c144e1
김문희 201611057 <ansgml9884@naver.com>
2016-04-18 17:18:10 +0900

    Update and rename w7Main2.py to w7Main.py

--- w7Main.py
+++ w7Main.py
@@ -1,7 +1,5 @@
 import turtle 
-
 wn=turtle.Screen() 
-
 t1=turtle.Turtle() 
 
 def drawSquareAt(size, pos): 
@@ -15,16 +13,19 @@ def drawSquareAt(size, pos):
         t1.right(90) 
     return tracks 
 
+def drawSquareFrom(tracks):
+    for i in range(0,5):
+        t1.goto(tracks[i])
+
 def lab7b(): 
     size=100 
     pos=t1.pos() 
-    mytrack=drawSquareAt(size,pos) 
-    print mytrack 
+    mytrack1=drawSquareAt(size,pos) 
+    print mytrack1
 
 def lab7c():
     tracks=((0,0),(180,0),(180,-250),(0,-250),(0,0))
-    for i in range(0,5):
-        t1.goto(tracks[i])
+    mytrack2=drawSquareFrom(tracks)
     
 def main():
     lab7b()
