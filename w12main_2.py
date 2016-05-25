48e5ca9b56c382dfdee1bcb93f1575d5f394f33f
김문희 201611057 <ansgml9884@naver.com>
2016-05-26 02:37:38 +0900

    line->LINE 바 줄 앞에 수정한 사람, 날짜, 시간 넣기

---
+++ w12main_2.py
@@ -0,0 +1,47 @@
+def myfile_1():
+    import time
+    myfile=open('output.txt','w') 
+    line1='first line\n' 
+    myfile.write(line1) 
+    line2='second line\n' 
+    myfile.write(line2) 
+    line3="third" 
+    myfile.write(line3) 
+    myfile.close()
+
+    fin=open('output.txt','r')
+    fout=open('outputUpper.txt','w')
+    for line in fin:
+        words=line.split()
+        if line.find('line')>=0:
+            msg='[kmh edited {0}]'.format(time.strftime('%Y-%m-%d %H:%M:%S'))
+            fout.write(msg)
+        for word in words:
+            if word=='line':
+                word=word.upper()
+            fout.write(word)
+            fout.write(" ")
+        fout.write('\n')
+    fin.close()
+    fout.close()
+    
+def myfile_2():
+    data=[1,2,3,4,5,6,7,8,9,10]
+    fout=open('outputNumber.txt','w')
+    for i in data:
+        str='{0}\t'.format(i)
+        fout.write(str)
+        if not i%2:
+            fout.write('\n')
+    fout.close()
+
+def lab12_2():
+    myfile_1()
+    myfile_2()
+
+def main():
+    lab12_2()
+
+if __name__=="__main__":
+    main() 
+
